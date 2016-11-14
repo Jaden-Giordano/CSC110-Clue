f1e705b6ad04985c50c92dc4c92ffc89689e075b
Caleb Gentz <cgentz@student.neumont.edu>
2016-11-14 13:29:18 -0700

    Getters and setters for player

--- src/main/java/edu/neumont/teamgift/clue/Player.java
+++ src/main/java/edu/neumont/teamgift/clue/Player.java
@@ -1,5 +1,25 @@
 package edu.neumont.teamgift.clue;
 
-class Player {
 
+
+
+
+
+class Player {
+	
+	
+	
+	
+	public String getName(){
+		return name;
+	}
+	public void setCharacterName(String Name){
+		name = Name;
+	}
+	public int getid(){
+		return id;
+	}
+	public void setid(int ID){
+		id = ID;
+	}
 }
