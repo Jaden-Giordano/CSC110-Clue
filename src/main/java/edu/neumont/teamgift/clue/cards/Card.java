ebe116c13ecc9f1849be928ba264416311490d10
Nathan Gift <NGift@student.neumont.edu>
2016-11-14 14:03:58 -0700

    card update

--- src/main/java/edu/neumont/teamgift/clue/cards/Card.java
+++ src/main/java/edu/neumont/teamgift/clue/cards/Card.java
@@ -2,13 +2,12 @@ package edu.neumont.teamgift.clue.cards;
 
 public class Card {
 	private String name;
-<<<<<<< HEAD
-	public String getName(){
+
+	public String getName() {
 		return name;
 	}
-	public void setName(String Name){
+
+	public void setName(String Name) {
 		name = Name;
 	}
-=======
->>>>>>> branch 'master' of https://github.com/Jaden-Giordano/CSC110-Clue.git
 }
