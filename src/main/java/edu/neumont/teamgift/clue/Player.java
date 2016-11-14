86b1d0b1c955c99661a7b3efa5e6edcc7b9c8061
Nathan Gift <NGift@student.neumont.edu>
2016-11-14 13:28:02 -0700

    created player class and properties of players

--- src/main/java/edu/neumont/teamgift/clue/Player.java
+++ src/main/java/edu/neumont/teamgift/clue/Player.java
@@ -1,5 +1,12 @@
 package edu.neumont.teamgift.clue;
 
-class Player {
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.neumont.teamgift.clue.cards.Card;
+
+class Player {
+	private int id;
+	private String name;
+	private List<Card> cards = new ArrayList<>();
 }
