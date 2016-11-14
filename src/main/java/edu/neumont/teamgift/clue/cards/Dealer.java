86eef554bc62fd99b6cfeaf32a845d574274e54b
Caleb Gentz <cgentz@student.neumont.edu>
2016-11-14 14:20:32 -0700

    worked on dealer

--- src/main/java/edu/neumont/teamgift/clue/cards/Dealer.java
+++ src/main/java/edu/neumont/teamgift/clue/cards/Dealer.java
@@ -1,5 +1,15 @@
 package edu.neumont.teamgift.clue.cards;
 
-public class Dealer {
+import java.util.ArrayList;
 
+public class Dealer {
+	public void createCards() {
+		ArrayList<Card> cards = new ArrayList<Card>();
+
+	}
+
+	public void shuffleCards() {
+
+	}
+	
 }
