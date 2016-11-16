8f83ea2f246eeeb0cfab6f41e389201fa1a96ef6
Caleb Gentz <cgentz@student.neumont.edu>
2016-11-16 13:04:45 -0700

    Added Die class

--- src/main/java/edu/neumont/teamgift/clue/Clue.java
+++ src/main/java/edu/neumont/teamgift/clue/Clue.java
@@ -1,24 +1,22 @@
 package edu.neumont.teamgift.clue;
 
 import edu.neumont.teamgift.clue.cards.Dealer;
+import edu.neumont.teamgift.clue.cards.Die;
 import edu.neumont.teamgift.clue.front.FrontEndManager;
 
 public class Clue {
 
 	public static void main(String[] args) {
-		Dealer d = new Dealer();
-		d.createCards();
-		d.printCards();
-		System.out.println("\n");
-		d.shuffle();
-		d.printCards();
-		d.setKiller();
-		System.out.println("\n");
-		d.printKillerCaseFile();
-		System.out.println("\n");
-		d.printCards();
+		/*
+		 * Dealer d = new Dealer(); d.createCards(); d.printCards();
+		 * System.out.println("\n"); d.shuffle(); d.printCards(); d.setKiller();
+		 * System.out.println("\n"); d.printKillerCaseFile();
+		 * System.out.println("\n"); d.printCards();
+		 */
+		Die d = new Die();
+		System.out.println(d.rollDie());
 
-		new FrontEndManager();
+//		new FrontEndManager();
 	}
 
 }
