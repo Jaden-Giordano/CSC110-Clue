fa4dde6fb6cf582bc158620627223db3c433eb02
Jaden-Giordano <thejadenjack@gmail.com>
2016-11-17 13:27:56 -0700

    fixing project for eclipse

--- src/main/java/edu/neumont/teamgift/clue/Vector2i.java
+++ src/main/java/edu/neumont/teamgift/clue/Vector2i.java
@@ -33,7 +33,7 @@ public class Vector2i implements Comparable<Vector2i> {
      * Add two vectors together using basic vector addition.
      * @param a The first Vector.
      * @param b The second Vector.
-     * @return Returns the sum of the vectors
+     * @return Returns the sum of the vectors.
      */
     static Vector2i add(final Vector2i a, final Vector2i b) {
         return new Vector2i(a.x + b.x, a.y + b.y);
@@ -53,36 +53,58 @@ public class Vector2i implements Comparable<Vector2i> {
      * Multiply two vectors using basic vector multiplication.
      * @param a The first vector.
      * @param b The second vector.
-     * @return
+     * @return Returns the product of the vectors.
      */
     static Vector2i multiply(final Vector2i a, final Vector2i b) {
         return new Vector2i(a.x * b.x, a.y * b.y);
     }
 
-    public static Vector2i divide(Vector2i a, Vector2i b) {
+    /**
+     * Divides two vectors using basic vector division.
+     * @param a The first vector.
+     * @param b The second vector.
+     * @return Returns the quotient of the vectors.
+     */
+    public static Vector2i divide(final Vector2i a, final Vector2i b) {
         if (b.x == 0 || b.y == 0)
             throw new ArithmeticException("Cannot divide by 0.");
 
         return new Vector2i(a.x / b.x, a.y / b.y);
     }
 
-    static int distance(Vector2i a, Vector2i b) {
+    /**
+     * Gets the distance between two points, disregarding diagonals.
+     * @param a The starting vector.
+     * @param b The ending vector.
+     * @return Returns the distance between the two vectors.
+     */
+    public static int distance(final Vector2i a, final Vector2i b) {
         return (b.x - a.x) + (b.y - a.y);
     }
 
-    public Vector2i add(Vector2i b) {
+    /**
+     * Adds this vector to another.
+     * @param b The other vector.
+     * @return A new vector of the sum.
+     */
+    public final Vector2i add(final Vector2i b) {
         return add(this, b);
     }
 
-    public Vector2i sub(Vector2i b) {
+    /**
+     * Subtracts this vector by another.
+     * @param b The other vector
+     * @return A new vector of the
+     */
+    public final Vector2i sub(final Vector2i b) {
         return sub(this, b);
     }
 
-    public Vector2i multiply(Vector2i b) {
+    public final Vector2i multiply(final Vector2i b) {
         return multiply(this, b);
     }
 
-    public Vector2i divide(Vector2i b) throws ArithmeticException {
+    public final Vector2i divide(final Vector2i b) throws ArithmeticException {
         return add(this, b);
     }
 
