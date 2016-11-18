1c8a8e2433c3b1c0c2a5e209652c6cbe86d10460
Jaden-Giordano <thejadenjack@gmail.com>
2016-11-18 10:48:01 -0700

    Reformatting/Commenting Die and GameMaster

--- src/main/java/edu/neumont/teamgift/clue/board/Die.java
+++ src/main/java/edu/neumont/teamgift/clue/board/Die.java
@@ -2,9 +2,24 @@ package edu.neumont.teamgift.clue.board;
 
 import java.util.Random;
 
+/**
+ * A simple die.
+ */
 public class Die {
-	public int rollDie(){
-		Random r = new Random();
-		return (r.nextInt(6) + 1) + (r.nextInt(6) + 1);
-	}
+
+    /**
+     * The amount of sides on the die.
+     */
+    private static final int SIDES_COUNT = 6;
+
+    /**
+     * Get a random face value on the die.
+     *
+     * @return The side of the die rolled.
+     */
+    public final int rollDie() {
+        Random r = new Random();
+        return (r.nextInt(SIDES_COUNT) + 1) + (r.nextInt(SIDES_COUNT) + 1);
+    }
+
 }
--- src/main/java/edu/neumont/teamgift/clue/board/GameMaster.java
+++ src/main/java/edu/neumont/teamgift/clue/board/GameMaster.java
@@ -2,44 +2,72 @@ package edu.neumont.teamgift.clue.board;
 
 import edu.neumont.teamgift.clue.Player;
 
+/**
+ * A controller for all aspects of game.
+ */
 public class GameMaster {
-	// TODO Prompt for number of players
-	private int numPlayers = 0;
-	private Player[] playerList = new Player[numPlayers];
-	// TODO Prompt for player select if we want
-	private String[] people = { "Colonel Mustard", "Miss Scarlet", "Professor Plum", "Mr. Green", "Mrs. White",
-			"Mrs. Peacock" };
-	private Board board;
+    /**
+     * The amount of players in the game.
+     * TODO Prompt for number of players
+     */
+    private int numPlayers = 0;
+    /**
+     * The list of players in the game.
+     */
+    private Player[] playerList = new Player[numPlayers];
 
-	public void createBoard() {
-		// TODO Get the path that will be used.
-		// board = new Board();
-	}
+    /**
+     * The people available in the game to choose from.
+     * TODO Prompt for player select if we want
+     */
+    private String[] people = {"Colonel Mustard", "Miss Scarlet", "Professor Plum", "Mr. Green", "Mrs. White",
+            "Mrs. Peacock"};
 
-	public void createPlayers() {
-		for (int i = 0; i < numPlayers; i++) {
-			Player p = new Player(board, i + 1, people[i]);
-			playerList[i] = p;
-		}
-	}
+    /**
+     * The game board.
+     */
+    private Board board;
 
-	public void takeTurn(Player p) {
-		Die d = new Die();
-		int roll = d.rollDie();
-		boolean checkValidMove = false;
-		while (!checkValidMove) {
-			checkValidMove = true;
-			// TODO get Vector2i form somewhere
-			// TODO change null to Vector2i
-			int spaceLeft = p.makeMove(null, roll);
-			if (spaceLeft == -1) {
-				checkValidMove = false;
-			} else if (spaceLeft > 0) {
-				checkValidMove = false;
-				int rollReamining = roll - spaceLeft;
-				// TODO get new move location
-			}
-		}
-	}
+    /**
+     * Creates the game board.
+     */
+    public final void createBoard() {
+        // TODO Get the path that will be used.
+        // board = new Board();
+    }
+
+    /**
+     * Creates all the players in the game, and stores them into player list.
+     */
+    public final void createPlayers() {
+        for (int i = 0; i < numPlayers; i++) {
+            Player p = new Player(board, i + 1, people[i]);
+            playerList[i] = p;
+        }
+    }
+
+    /**
+     * Handles the players turn and its movement.
+     *
+     * @param p The player that is currently running its turn.
+     */
+    public final void takeTurn(final Player p) {
+        Die d = new Die();
+        int roll = d.rollDie();
+        boolean checkValidMove = false;
+        while (!checkValidMove) {
+            checkValidMove = true;
+            // TODO get Vector2i form somewhere
+            // TODO change null to Vector2i
+            int spaceLeft = p.makeMove(null, roll);
+            if (spaceLeft == -1) {
+                checkValidMove = false;
+            } else if (spaceLeft > 0) {
+                checkValidMove = true;
+                int rollReamining = roll - spaceLeft;
+                // TODO get new move location
+            }
+        }
+    }
 
 }
