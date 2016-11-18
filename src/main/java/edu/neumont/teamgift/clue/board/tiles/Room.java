2f415d7b8d3a0044d029ab127fbd555d20275789
Jaden-Giordano <thejadenjack@gmail.com>
2016-11-18 10:20:11 -0700

    Reformatting/Commenting Room file

--- src/main/java/edu/neumont/teamgift/clue/board/tiles/Room.java
+++ src/main/java/edu/neumont/teamgift/clue/board/tiles/Room.java
@@ -6,19 +6,40 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * A room consisting of a set of tiles. Movement in this
+ * each room is one to move in and one to move out (acts as single tile).
+ */
 public class Room {
 
+    /**
+     * List of all the tiles that make up a room.
+     */
     private List<Tile> tiles;
 
+    /**
+     * Create the container for the room.
+     */
     public Room() {
         tiles = new ArrayList<>();
     }
 
-    public void addTile(Tile t) {
+    /**
+     * Add a tile that makes up a room.
+     *
+     * @param t The tile to be added.
+     */
+    public final void addTile(final Tile t) {
         tiles.add(t);
     }
 
-    public Tile getTile(Vector2i position) {
+    /**
+     * Get a tile at a specific position.
+     *
+     * @param position The position of the tile.
+     * @return The tile correlating with the given coordinates.
+     */
+    public final Tile getTile(final Vector2i position) {
         for (Tile i : tiles) {
             if (i.getPosition() == position) {
                 return i;
@@ -27,7 +48,14 @@ public class Room {
         return null;
     }
 
-    public <T> T[] getTiles(Class<? extends Tile> tile) {
+    /**
+     * Get all tiles of specific type.
+     *
+     * @param tile The type of tile to look for.
+     * @param <T>  A tile class, or sub-class.
+     * @return All the tiles that match the type.
+     */
+    public final <T> T[] getTiles(final Class<? extends Tile> tile) {
         //noinspection unchecked
         return (T[]) tiles.stream().filter(i -> i.getClass() == tile).collect(Collectors.toList()).toArray();
     }
