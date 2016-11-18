7c6d4cb750137e3efc901d94eadc0021491fafa5
Jaden-Giordano <thejadenjack@gmail.com>
2016-11-18 10:03:34 -0700

    Reformatting/Commenting TileRegistry file

--- src/main/java/edu/neumont/teamgift/clue/TileRegistry.java
+++ src/main/java/edu/neumont/teamgift/clue/TileRegistry.java
@@ -6,9 +6,25 @@ import edu.neumont.teamgift.clue.board.tiles.Empty;
 import edu.neumont.teamgift.clue.board.tiles.Pathway;
 import edu.neumont.teamgift.clue.board.tiles.Tile;
 
-public class TileRegistry {
+/**
+ * Registry containing functions for getting tiles based on ids.
+ */
+public final class TileRegistry {
 
-    public static Tile getTile(Board b, int id) {
+    /**
+     * Tile registry
+     */
+    private TileRegistry() {
+    }
+
+    /**
+     * Get tile based on id.
+     *
+     * @param b  The board the tile will be on.
+     * @param id The id of the tile to add.
+     * @return A new tile based on specific id.
+     */
+    public static Tile getTile(final Board b, final int id) {
         if (id == 1) {
             return new Pathway(b);
         } else if (id == 2) {
