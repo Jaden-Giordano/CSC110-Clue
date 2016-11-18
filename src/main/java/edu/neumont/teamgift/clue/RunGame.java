713c51a9e4e533980c96eb7e3e052839c6658155
Caleb Gentz <cgentz@student.neumont.edu>
2016-11-18 13:36:53 -0700

    Setup run game flow

---
+++ src/main/java/edu/neumont/teamgift/clue/RunGame.java
@@ -0,0 +1,17 @@
+package edu.neumont.teamgift.clue;
+
+public class RunGame {
+	public void runGame() {
+		// TODO Number of players
+		// TODO Name selection
+		// TODO Create players created by above
+		// TODO Create notepad
+		// TODO Create board
+		// TODO Create cards
+		// TODO Shuffle cards
+		// TODO Set killer case file
+		// TODO Deal rest of cards
+		// TODO Take turns through players
+		// TODO Win condition
+	}
+}
--- src/main/java/edu/neumont/teamgift/clue/board/GameMaster.java
+++ src/main/java/edu/neumont/teamgift/clue/board/GameMaster.java
@@ -7,75 +7,79 @@ import edu.neumont.teamgift.clue.Player;
  */
 @SuppressWarnings("unused")
 public class GameMaster {
-    /**
-     * The amount of players in the game.
-     * TODO Prompt for number of players
-     */
-    @SuppressWarnings("CanBeFinal")
-    private int numPlayers = 0;
-    /**
-     * The list of players in the game.
-     * TODO Make sure to resize array based on amount of players.
-     */
-    @SuppressWarnings({"MismatchedReadAndWriteOfArray", "CanBeFinal"})
-    private Player[] playerList = new Player[numPlayers];
+	/**
+	 * The amount of players in the game. TODO Prompt for number of players
+	 */
+	@SuppressWarnings("CanBeFinal")
+	private int numPlayers = 0;
+	/**
+	 * The list of players in the game. TODO Make sure to resize array based on
+	 * amount of players.
+	 */
+	@SuppressWarnings({ "MismatchedReadAndWriteOfArray", "CanBeFinal" })
+	private Player[] playerList = new Player[numPlayers];
 
-    /**
-     * The people available in the game to choose from.
-     * TODO Prompt for player select if we want
-     */
-    private final String[] people = {"Colonel Mustard", "Miss Scarlet",
-            "Professor Plum", "Mr. Green", "Mrs. White",
-            "Mrs. Peacock"};
+	/**
+	 * The people available in the game to choose from. TODO Prompt for player
+	 * select if we want
+	 */
+	private final String[] people = { "Colonel Mustard", "Miss Scarlet", "Professor Plum", "Mr. Green", "Mrs. White",
+			"Mrs. Peacock" };
 
-    /**
-     * The game board.
-     */
-    @SuppressWarnings("CanBeFinal")
-    private Board board;
+	/**
+	 * The game board.
+	 */
+	@SuppressWarnings("CanBeFinal")
+	private Board board;
 
-    /**
-     * Creates the game board.
-     */
-    @SuppressWarnings("EmptyMethod")
-    public final void createBoard() {
-        // TODO Get the path that will be used.
-        // board = new Board();
-    }
+	/**
+	 * Creates the game board.
+	 */
+	@SuppressWarnings("EmptyMethod")
+	public final void createBoard() {
+		// TODO Get the path that will be used.
+		// board = new Board();
+	}
 
-    /**
-     * Creates all the players in the game, and stores them into player list.
-     */
-    public final void createPlayers() {
-        for (int i = 0; i < numPlayers; i++) {
-            Player p = new Player(board, i + 1, people[i]);
-            playerList[i] = p;
-        }
-    }
+	/**
+	 * Creates all the players in the game, and stores them into player list.
+	 */
+	public final void createPlayers() {
+		for (int i = 0; i < numPlayers; i++) {
+			Player p = new Player(board, i + 1, people[i]);
+			playerList[i] = p;
+		}
+	}
 
-    /**
-     * Handles the players turn and its movement.
-     *
-     * @param p The player that is currently running its turn.
-     */
-    public final void takeTurn(final Player p) {
-        Die d = new Die();
-        int roll = d.rollDie();
-        boolean checkValidMove = false;
-        while (!checkValidMove) {
-            checkValidMove = true;
-            // TODO get Vector2i form somewhere
-            // TODO change null to Vector2i
-            int spaceLeft = p.makeMove(null, roll);
-            if (spaceLeft == -1) {
-                checkValidMove = false;
-            } else if (spaceLeft > 0) {
-                checkValidMove = true;
-                @SuppressWarnings("UnusedAssignment")
-                int rollRemaining = roll - spaceLeft;
-                // TODO get new move location
-            }
-        }
-    }
+	/**
+	 * Handles the players turn and its movement.
+	 *
+	 * @param p
+	 *            The player that is currently running its turn.
+	 */
+	public final void takeTurn(final Player p) {
+		// TODO add options: roll dice or travel through secret passage if
+		// present
+		// TODO Check if doors are blocked
+		Die d = new Die();
+		int roll = d.rollDie();
+		boolean checkValidMove = false;
+		while (!checkValidMove) {
+			checkValidMove = true;
+			// TODO output to user how many moves
+			// TODO get Vector2i form somewhere
+			// TODO change null to Vector2i
+			int spaceLeft = p.makeMove(null, roll);
+			if (spaceLeft == -1) {
+				checkValidMove = false;
+				// TODO output invalid move
+			} else if (spaceLeft > 0) {
+				checkValidMove = false;
+				roll -= spaceLeft;
+				// @SuppressWarnings("UnusedAssignment")
+				// TODO get new move location
+			}
+		}
+	}
 
 }
