00e6920cf92613287d6467f51bbc6d8f2cae2274
Caleb Gentz <cgentz@student.neumont.edu>
2016-11-19 18:16:19 -0700

    Formating

--- src/main/java/edu/neumont/teamgift/clue/RunGame.java
+++ src/main/java/edu/neumont/teamgift/clue/RunGame.java
@@ -1,10 +1,7 @@
 package edu.neumont.teamgift.clue;
 
-import edu.neumont.teamgift.clue.board.Board;
 import edu.neumont.teamgift.clue.board.GameMaster;
-import edu.neumont.teamgift.clue.cards.Card;
 import edu.neumont.teamgift.clue.cards.Dealer;
-import edu.neumont.teamgift.clue.cards.Deck;
 
 public class RunGame {
 	public void runGame() {
@@ -12,13 +9,13 @@ public class RunGame {
 		Dealer dealer = new Dealer();
 		// TODO Number of players
 		// TODO Name selection if we want
-		// Create players
+		// Create cards
 		dealer.createCards();
 		// Shuffle cards
 		dealer.shuffle();
 		// Set killer case file
 		dealer.setKiller();
-		// TODO Deal rest of cards
+		// Create players
 		game.createPlayers();
 		/*
 		 * for (int i = 0; i < game.getPlayerList().length; i++) {
@@ -26,25 +23,17 @@ public class RunGame {
 		 * " This is round " + i); }
 		 */
 		// Create board
-		// game.createBoard();
-		// Create cards
+		game.createBoard();
 		// dealer.printCards();
+		// Deal rest of cards
 		dealer.dealCards(game);
-
-		for (int i = 0; i < game.getPlayerList().length; i++) {
-			System.out.println("Player " + game.getPlayerList()[i].getID() + ": " + game.getPlayerList()[i].getName());
-			for (int j = 0; j < game.getPlayerList()[i].getDeck().size(); j++) {
-				System.out.println(game.getPlayerList()[i].getDeck().get(j).getName());
-			}
-			System.out.println("\n");
-		}
 		// TODO Win condition
 		// TODO Take turns through players
-		// while (true) {
-		// for (int i = 0; i < game.getPlayerList().length; i++) {
-		// game.takeTurn(game.getPlayerList()[i]);
-		// }
-		// }
+		while (true) {
+			for (int i = 0; i < game.getPlayerList().length; i++) {
+				game.takeTurn(game.getPlayerList()[i]);
+			}
+		}
 
 	}
 }
