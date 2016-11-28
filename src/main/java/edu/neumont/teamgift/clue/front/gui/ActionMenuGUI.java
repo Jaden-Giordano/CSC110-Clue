b7e466481acdc49d0e177202e542ff14451a8fab
Timothy_Gunter <tgunter@student.neumont.edu>
2016-11-28 14:07:07 -0700

    Set up skeleton of the Action menu gui

--- src/main/java/edu/neumont/teamgift/clue/front/gui/ActionMenuGUI.java
+++ src/main/java/edu/neumont/teamgift/clue/front/gui/ActionMenuGUI.java
@@ -1,5 +1,8 @@
 package edu.neumont.teamgift.clue.front.gui;
 
+import java.awt.GridLayout;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.WindowConstants;
 
@@ -13,5 +16,23 @@ public class ActionMenuGUI extends JFrame {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setSize(1440, 2200);
 		this.setVisible(true);
+	}
+	private void createActionButtons(){
+		setLayout(new GridLayout(0,1));
+		JButton rollForTurn = new JButton("Roll Dice");
+		rollForTurn.addActionListener();
+		add(rollForTurn);
+		JButton moveForTurn = new JButton("Move");
+		moveForTurn.addActionListener();
+		add(moveForTurn);
+		JButton beSuspicious = new JButton("Suspicion");
+		beSuspicious.addActionListener();
+		add(beSuspicious);
+		JButton objection = new JButton("Make Accusation");
+		objection.addActionListener();
+		add(objection);
+		JButton passTurn = new JButton("End Turn");
+		passTurn.addActionListener();
+		add(passTurn);
 	}
 }
