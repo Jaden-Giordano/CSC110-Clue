826026202a91aed497041aa93e0f532fb9fc5b3e
Timothy_Gunter <tgunter@student.neumont.edu>
2016-11-29 16:42:22 -0700

    Large gui stuff....again

--- src/main/java/edu/neumont/teamgift/clue/front/gui/LargeGUI.java
+++ src/main/java/edu/neumont/teamgift/clue/front/gui/LargeGUI.java
@@ -1,5 +1,6 @@
 package edu.neumont.teamgift.clue.front.gui;
 
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,18 +13,21 @@ import javax.swing.JPanel;
 public class LargeGUI extends JFrame{
 	
 	public void createAwkwardlyLargeGUI(){
-    	LargeGUI gui = new LargeGUI();
-    	gui.setSize(3000, 2000);
-    	gui.setVisible(true);
-    	gui.setLayout(new GridLayout(2, 1));
+    	//LargeGUI gui = new LargeGUI();
+    	this.setSize(3000, 2000);
+    	this.setVisible(true);
+    	this.setLayout(new GridLayout(2, 1));
     	
     }
 	public void setupAwkwardlyLargueGUI(){
+		Font titleFont = new Font("Serif", Font.BOLD, 80);
 		JPanel willYouPlsWork = new JPanel();
 		add(willYouPlsWork);
 		JLabel playersTurn = new JLabel("It is the next player's turn");
+		playersTurn.setFont(titleFont);
     	willYouPlsWork.add(playersTurn);
     	JButton turnSwap = new JButton("Continue");
+    	turnSwap.setFont(titleFont);
     	turnSwap.addActionListener(new ActionListener(){
     		
     		public void actionPerformed(ActionEvent e){
@@ -33,7 +37,7 @@ public class LargeGUI extends JFrame{
     	willYouPlsWork.add(turnSwap);
 	}
 	public void callMeMaybe(){
-		createAwkwardlyLargeGUI();
 		setupAwkwardlyLargueGUI();
+		createAwkwardlyLargeGUI();
 	}
 }
